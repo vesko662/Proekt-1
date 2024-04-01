517a4f60d518a04fc0547d2ee05d9ddfda7ddc2c
vesko662 <vesko662@abv.bg>
2024-04-01 21:42:36 +0300

    Added save commmand.

--- src/main/File/FileWrite.java
+++ src/main/File/FileWrite.java
@@ -1,7 +1,16 @@
 package main.File;
 
+import java.io.*;
+
 public class FileWrite {
 
-
+    public void writeFile(String filePath,String content)
+    {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filePath))) {
+            bw.write(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
--- src/main/commands/SaveCommand.java
+++ src/main/commands/SaveCommand.java
@@ -1,10 +1,14 @@
 package main.commands;
 
+import main.File.FileWrite;
 import main.contracts.Command;
+import main.singletons.FileData;
 
 public class SaveCommand implements Command {
     @Override
     public void execute(String args) {
-
+        FileWrite fw=new FileWrite();
+        FileData fd=FileData.getInstance();
+        fw.writeFile(fd.getFilePath(),fd.getFileData());
     }
 }
